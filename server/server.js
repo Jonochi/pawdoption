25979750f9e207fc28ee75d9a502dea2632c70ad
Jonochi <tana.jayne@gmail.com>
2023-06-27 14:48:02 -0600

    Undo incorrect Heroku connections

--- server/server.js
+++ server/server.js
@@ -4,7 +4,6 @@ const path = require('path');
 const { authMiddleware } = require('./utils/auth');
 const { typeDefs, resolvers } = require('./schemas');
 const db = require('./config/connection');
-const uri = process.env.MONGODB_URI;
 const stripe = require('stripe')('sk_test_51N9HY7Ggv41H9K2AMU9Ssooe1aPIPmVh9gQQyZ3igQdpFkDcIfUoXQC6jCAXam5KJRCcppwxeRyvINyPj2mAbUKk00ZRgRNR7U')
 
 const YOUR_DOMAIN = process.env.YOUR_DOMAIN || 'http://localhost:3000'
@@ -40,11 +39,6 @@ app.post('/create-checkout-session', async (req, res) => {
   });
   res.redirect(303, session.url);
 });
-
-mongoose
-  .connect(uri, { useUnifiedTopology: true, useNewUrlParser: true })
-  .then(() => console.log('MongoDB connected!'))
-  .catch(err => console.log('Error:- ' + err));
 
 // Create a new instance of an Apollo server with the GraphQL schema
 const startApolloServer = async () => {
